ef9c1d5d5bcd8144c766ae8511c94e4fa9874530
Vasiliy Kletkin <v.kletkin@mail.ru>
2022-06-23 15:59:25 +0300

    add models

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ("text", "pub_date", "author") 
+    # добавляем интерфейс для поиска по тексту постов
+    search_fields = ("text",) 
+    # добавляем возможность фильтрации по дате
+    list_filter = ("pub_date",) 
+    empty_value_display = "-пусто-"
+
+admin.site.register(Post, PostAdmin) 
--- post/models.py
+++ post/models.py
@@ -1,3 +1,30 @@
+import imp
+from unicodedata import name
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='author_group')
+
+    def __str__(self):
+        return self.name[:100]
+
+
+class Post(models.Model):
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='author_posts')
+    group = models.ForeignKey(Group, on_delete=models.CASCADE,
+                              related_name='group_posts', blank=True, null=True)
+    url = models.URLField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+
+    def __str__(self):
+        return self.image[:100]
